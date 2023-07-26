42fdc0b69f08fd692654e27cd3300b124726dff5
gokhanmeteerturk <me@e-mete.com>
2023-07-26 14:36:19 +0800

    Added endpoint for submission creation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from cursor import Database
-from payloads import RedditUserPayload
+from payloads import RedditUserPayload, RedditPostPayload
 
 
 limiter = Limiter(key_func=get_remote_address)
@@ -15,9 +15,11 @@ app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/init/")
 @limiter.limit("5/minute")
@@ -27,13 +29,15 @@ def initialize(request: Request):
     master_key = initialize_once()
     if master_key is None:
         raise HTTPException(status_code=404, detail="Not Found")
-    return {'master_key':master_key}
+    return {"master_key": master_key}
+
 
 @app.post("/test/")
 @limiter.limit("1000/minute")
 def test(request: Request):
     check_key(request)
-    return {"result":"Success"}
+    return {"result": "Success"}
+
 
 # @app.post("/post/")
 # @limiter.limit("1000/minute")
@@ -41,16 +45,49 @@ def test(request: Request):
 #     check_key(request)
 #     return {"result":"Success"}
 
+
 @app.post("/reddit_user/")
 @limiter.limit("20/minute")
-def create_reddit_user(request: Request, user:RedditUserPayload):
+def create_reddit_user(request: Request, user: RedditUserPayload):
     check_key(request)
     db = Database()
     c = db.connect()
-    c.execute("INSERT INTO users VALUES(?, ?, ?, ?)",(user.username, user.password, user.client_id, user.client_secret))
+    c.execute(
+        "INSERT INTO users VALUES(?, ?, ?, ?)",
+        (user.username, user.password, user.client_id, user.client_secret),
+    )
     db.connection.commit()
     db.disconnect()
-    return {"result":"Success"}
+    return {"result": "Success"}
+
+
+@app.post("/scheduled_submission/")
+@limiter.limit("100/minute")
+def create_reddit_scheduled_submission(request: Request, p: RedditPostPayload):
+    check_key(request)
+    db = Database()
+    c = db.connect()
+    c.execute(
+        "INSERT INTO submissions VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        (
+            p.planned_unix_datetime,
+            p.status,
+            p.username,
+            p.sub,
+            p.title,
+            p.text,
+            p.link,
+            p.image_name,
+            p.video,
+            p.flairid,
+            p.nsfw,
+            p.submission_id,
+        ),
+    )
+    db.connection.commit()
+    db.disconnect()
+    return {"result": "Success"}
+
 
 @app.get("/reddit_user/{username}/")
 def read_reddit_user(request: Request, username: str):
@@ -58,7 +95,10 @@ def read_reddit_user(request: Request, username: str):
     user = None
     db = Database()
     c = db.connect()
-    c.execute("SELECT * FROM users WHERE username = ? ORDER BY rowid ASC LIMIT 1", (username,))
+    c.execute(
+        "SELECT * FROM users WHERE username = ? ORDER BY rowid ASC LIMIT 1",
+        (username,),
+    )
     user_tuple = c.fetchone()
     db.disconnect()
     if user_tuple:
@@ -67,13 +107,17 @@ def read_reddit_user(request: Request, username: str):
         raise HTTPException(status_code=404, detail="Not Found")
     return user
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 def check_key(request):
-    key = request.headers.get('authorization')
-    if key is None or not Database().is_master_key_correct(remove_prefix(key,'Basic ')):
+    key = request.headers.get("authorization")
+    if key is None or not Database().is_master_key_correct(
+        remove_prefix(key, "Basic ")
+    ):
         raise HTTPException(status_code=401, detail="Unauthorized")
 
 
@@ -86,7 +130,8 @@ def check_scheduled_submissions():
     except Exception as e:
         print(e)
 
+
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
-        return text[len(prefix):]
+        return text[len(prefix) :]
     return text  # or whatever
