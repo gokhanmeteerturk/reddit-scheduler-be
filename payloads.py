f0ae871610e8f39f1ea7afd907c1843033b7bffe
gokhanmeteerturk <me@e-mete.com>
2023-07-30 21:28:24 +0800

    Added missing rows & rowid

--- payloads.py
+++ payloads.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseModel
 
 
@@ -18,7 +19,18 @@ class RedditUserPayload(CustomBaseModel):
     client_secret: str
 
 
+
+class RedditUsersListPayload(BaseModel):
+    page: int
+    per_page: int
+    count: int
+    users: List[RedditUserPayload]
+
+
 class RedditPostPayload(CustomBaseModel):
+    rowid: int | None = None
+    planned_unix_datetime: int | None = None
+    status: str | None = None
     username: str
     sub: str
     title: str
@@ -28,3 +40,11 @@ class RedditPostPayload(CustomBaseModel):
     video: str | None = None
     flairid: str | None = None
     nsfw: bool | None = None
+    submission_id: str | None = None
+
+
+class RedditPostsListPayload(BaseModel):
+    page: int
+    per_page: int
+    count: int
+    users: List[RedditPostPayload]
