bd454a74f552b5ef4da4979dc230af9f6c2e9b5a
me@e-mete.com <me@e-mete.com>
2023-07-31 15:32:02 +0800

    Fix connection leak

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ from cursor import Database
 
 
 def initialize_once():
-    if Database().is_table_available("auth", keep_alive=True):
+    if Database().is_table_available("auth", keep_alive=False):
         return None
     else:
         master_key = Database().initialize()
