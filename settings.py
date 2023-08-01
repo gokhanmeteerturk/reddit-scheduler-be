0c95a090c20c37b6967b64fd102f73228c395504
gokhanmeteerturk <me@e-mete.com>
2023-08-01 19:16:34 +0800

    img and video settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+IMG_DIR="./images/"
+VIDEO_DIR = "./videos/"
+
+IMG_MAX_FILE_SIZE = 20 * 1024 * 1024  # 20 MB in bytes
+VIDEO_MAX_FILE_SIZE = 20 * 1024 * 1024  # 20 MB in bytes
+ALLOWED_IMG_EXTENSIONS = ["image/png", "image/jpeg", "image/jpg", "image/gif", "image/webp"]
+ALLOWED_VIDEO_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv'}
