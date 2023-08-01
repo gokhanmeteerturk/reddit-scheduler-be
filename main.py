ff02561152e4ffbd21b15043047449abfe04034b
gokhanmeteerturk <me@e-mete.com>
2023-08-01 19:17:38 +0800

    Image upload endpoint with helpers

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import Depends, FastAPI, HTTPException, Request
+import os
+import shutil
+from fastapi import Depends, FastAPI, File, HTTPException, Request, UploadFile
+from fastapi.responses import JSONResponse
 
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from settings import ALLOWED_IMG_EXTENSIONS, IMG_MAX_FILE_SIZE
 
 from viewsets.users import router as users_router
 from viewsets.scheduled_submissions import router as scheduled_submissions_router
+from viewsets.helpers import check_key, create_thumbnail, is_valid_image, generate_unique_filename, is_valid_video, save_image, save_video
+
+from starlette_validation_uploadfile import ValidateUploadFileMiddleware
+
+
 
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
@@ -25,6 +34,49 @@ app.include_router(
     dependencies=[Depends(get_remote_address)],
 )
 
+
+app.add_middleware(
+    ValidateUploadFileMiddleware,
+    app_path=[
+        "/upload_image/"
+    ],
+    max_size=IMG_MAX_FILE_SIZE,
+    file_type=ALLOWED_IMG_EXTENSIONS
+)
+
+@app.post("/upload_image/")
+async def upload_image(request: Request, file: UploadFile = File(...)):
+    check_key(request)
+
+    image = is_valid_image(file)
+    if image is None:
+        return JSONResponse(content={"error": "Invalid image file."}, status_code=400)
+
+    new_filename = generate_unique_filename(file.filename)
+    save_image(image, new_filename)
+
+    thumbnail_filename = f"thumb_{new_filename}"
+    thumbnail = create_thumbnail(image)
+    save_image(thumbnail, thumbnail_filename)
+
+    return {"filename": new_filename}
+
+
+"""
+@app.add_route("/upload_video/", methods=["POST"])
+async def upload_video(request: Request, file: UploadFile = File(...)):
+    check_key(request)
+
+    # IMPORTANT: below function ain't doing anything right now!
+    video = is_valid_video(file)
+    if video is None:
+        return JSONResponse(content={"error": "Invalid video file."}, status_code=400)
+
+    new_filename = generate_unique_filename(file.filename)
+    save_video(video, new_filename)
+
+    return {"filename": new_filename}
+"""
 
 @app.get("/init/")
 @limiter.limit("5/minute")
--- viewsets/helpers.py
+++ viewsets/helpers.py
@@ -1,5 +1,12 @@
+import os
+import shutil
 from fastapi import HTTPException
 from cursor import Database
+from PIL import Image
+from hashids import Hashids
+import time
+
+from settings import IMG_DIR, VIDEO_DIR
 
 
 def remove_prefix(text, prefix):
@@ -14,3 +21,39 @@ def check_key(request):
         remove_prefix(key, "Basic ")
     ):
         raise HTTPException(status_code=401, detail="Unauthorized")
+
+def is_valid_image(file):
+    try:
+        image = Image.open(file.file)
+        image.verify()
+        return image
+    except:
+        return None
+
+def save_image(image, filename):
+    image.save(os.path.join(IMG_DIR, filename))
+
+def create_thumbnail(image, size=(96, 96)):
+    return image.copy().thumbnail(size)
+
+def generate_unique_filename(original_filename):
+    hashids = Hashids(salt="your_salt_here", min_length=8)
+    timestamp = int(time.time())
+    hashid = hashids.encode(timestamp)
+    _, extension = os.path.splitext(original_filename)
+    return f"{hashid}{extension}"
+
+def is_valid_video(file):
+    # TODO: add video validation logic. Opencv would be an overkill?
+    return file
+
+def save_video(video, filename):
+    with open(os.path.join(VIDEO_DIR, filename), "wb") as video_file:
+        shutil.copyfileobj(video.file, video_file)
+
+def generate_unique_filename(original_filename):
+    hashids = Hashids(salt="your_salt_here", min_length=8)
+    timestamp = int(time.time())
+    hashid = hashids.encode(timestamp)
+    _, extension = os.path.splitext(original_filename)
+    return f"{hashid}{extension}"
